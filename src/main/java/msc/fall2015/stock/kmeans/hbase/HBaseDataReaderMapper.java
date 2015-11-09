941e45e005ba825238f43554cca05f4fb42d05b8
Chathuri Wimalasena <chathuri@apache.org>
2015-11-09 11:00:13 -0500

    read data by according to given date period

--- src/main/java/msc/fall2015/stock/kmeans/hbase/HBaseDataReaderMapper.java
+++ src/main/java/msc/fall2015/stock/kmeans/hbase/HBaseDataReaderMapper.java
@@ -21,6 +21,7 @@
 
 package msc.fall2015.stock.kmeans.hbase;
 
+import org.apache.commons.math.stat.regression.SimpleRegression;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableMapper;
@@ -34,19 +35,42 @@ import java.util.NavigableMap;
 public class HBaseDataReaderMapper extends TableMapper<Text, Text> {
 
     public void map(ImmutableBytesWritable row, Result value, Context context) throws InterruptedException, IOException {
-        for(Map.Entry<byte[], NavigableMap<byte[], NavigableMap<Long, byte[]>>> columnFamilyMap : value.getMap().entrySet())
-        {
-            for (Map.Entry<byte[], NavigableMap<Long, byte[]>> entryVersion : columnFamilyMap.getValue().entrySet())
-            {
-                for (Map.Entry<Long, byte[]> entry : entryVersion.getValue().entrySet())
-                {
+        SimpleRegression regression;
+        for (Map.Entry<byte[], NavigableMap<byte[], NavigableMap<Long, byte[]>>> columnFamilyMap : value.getMap().entrySet()) {
+            regression = new SimpleRegression();
+            int count = 1;
+            for (Map.Entry<byte[], NavigableMap<Long, byte[]>> entryVersion : columnFamilyMap.getValue().entrySet()) {
+                for (Map.Entry<Long, byte[]> entry : entryVersion.getValue().entrySet()) {
                     String rowKey = Bytes.toString(value.getRow());
                     String column = Bytes.toString(entryVersion.getKey());
                     byte[] val = entry.getValue();
                     String valOfColumn = new String(val);
                     System.out.println("RowKey : " + rowKey + " Column Key : " + column + " Column Val : " + valOfColumn);
+                    if (!valOfColumn.isEmpty()) {
+                        String[] priceAndCap = valOfColumn.split("_");
+                        if (priceAndCap.length > 1) {
+                            String pr = priceAndCap[0];
+                            if (pr != null && !pr.equals("null")){
+                                double price = Double.valueOf(pr);
+                                if (price < 0) {
+                                    price = price - 2 * price;
+                                }
+                                System.out.println("Price : " + price + " count : " + count);
+                                regression.addData(count, price);
+                            }
+                        }
+                    }
                 }
+                count++;
             }
+            // displays intercept of regression line
+            System.out.println("Intercept : " + regression.getIntercept());
+
+            // displays slope of regression line
+            System.out.println("Slope : " + regression.getSlope());
+
+            // displays slope standard error
+            System.out.println("Slope STD Error : " + regression.getSlopeStdErr());
         }
     }
 
